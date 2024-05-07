730c4a5d48ea420b4a600ba209c323a0672465c0
Marco Novaes <marco-novaes@uol.com.br>
2024-05-06 22:48:32 -0300

    Create string_4.py
    Forth commit

---
+++ string_4.py
@@ -0,0 +1,22 @@
+nome = "Marco"
+
+mensagem = f""" 
+    Olá meu nome é {nome},
+Eu estou aprendendo Python.
+    Essa mensagem tem diferentes recuos.
+"""
+
+print(mensagem)
+
+print(""" 
+    ============== MENU ================
+      
+      1 - Despositar
+      2 - Sacar
+      0 - Sair
+
+    ====================================
+
+      Obrigado por usar nosso sistema!!!!!
+
+""")
